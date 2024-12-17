51457dccbff6d81c991bc82d9b6e7be31611d4a7
Natan Pereira <natan.pereira@epitech.eu>
2024-12-17 10:53:19 +0100

    feat: When commiting to the VCS, now asks you if you want to verify the coding style before the commit.

--- src/main/java/io/github/natank25/epitechutils/chekinhandler/CodingStyleCheckinHandler.java
+++ src/main/java/io/github/natank25/epitechutils/chekinhandler/CodingStyleCheckinHandler.java
@@ -1,5 +1,8 @@
 package io.github.natank25.epitechutils.chekinhandler;
 
+import com.intellij.execution.ProgramRunnerUtil;
+import com.intellij.execution.RunManager;
+import com.intellij.execution.executors.DefaultRunExecutor;
 import com.intellij.openapi.application.Application;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
@@ -9,6 +12,7 @@ import com.intellij.openapi.vcs.checkin.CheckinHandler;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiManager;
+import io.github.natank25.epitechutils.module.EpitechDirectoryProjectGenerator;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -26,8 +30,18 @@ public class CodingStyleCheckinHandler extends CheckinHandler {
 	public ReturnResult beforeCheckin() {
 		VirtualFile coding_style_report_file = VfsUtil.findFile(Path.of(project.getBasePath(), "coding-style-reports.log"), true);
 		if (coding_style_report_file == null || !coding_style_report_file.exists()) {
-			Messages.showErrorDialog(project, "The coding style log file does not exists, please execute BANANA to continue", "Commit Blocked");
-			return ReturnResult.CANCEL;
+            int result = Messages.showYesNoDialog(project, "The coding style log file does not exists, do you want to verify the coding style ?", "Commit Blocked", Messages.getWarningIcon());
+			if (result == Messages.YES) {
+				var dockerRunConfig =
+						RunManager.getInstance(project).findConfigurationByName("Generate Coding Style Report");
+				if (dockerRunConfig == null) {
+					EpitechDirectoryProjectGenerator.createCodingStyleRunConfiguration(project);
+					dockerRunConfig =
+							RunManager.getInstance(project).findConfigurationByName("Generate Coding Style Report");
+				}
+				ProgramRunnerUtil.executeConfiguration(dockerRunConfig, DefaultRunExecutor.getRunExecutorInstance());
+				return ReturnResult.CANCEL;
+			}
 		}
 		ApplicationManager.getApplication().runWriteAction(() -> {
 			try {
